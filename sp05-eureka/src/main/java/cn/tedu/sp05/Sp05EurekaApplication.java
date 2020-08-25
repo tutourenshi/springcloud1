af7d95a759e39b2104cbad6cabcfbc50f3f38fb0
wang <736113959@qq.com>
2020-08-25 17:36:22 +0800

    eureka

--- sp05-eureka/src/main/java/cn/tedu/sp05/Sp05EurekaApplication.java
+++ sp05-eureka/src/main/java/cn/tedu/sp05/Sp05EurekaApplication.java
@@ -2,7 +2,9 @@ package cn.tedu.sp05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class Sp05EurekaApplication {
 
