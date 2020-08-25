19d6d645b2fa4f1fbebce7a12bd0e5487bf086a5
wang <736113959@qq.com>
2020-08-25 15:04:03 +0800

    springcloud01-sp05

---
+++ sp05-eureka/src/main/java/cn/tedu/sp05/Sp05EurekaApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp05EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp05EurekaApplication.class, args);
+    }
+
+}
